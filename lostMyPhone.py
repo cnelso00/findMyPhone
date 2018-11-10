c15ef25c30913d695035e3ff1cdbb85f9e822cf2
cnelso00 <cnelso3@gmu.edu>
2018-11-09 20:33:12 -0500

    it works. will prompt for user info if none present.

--- lostMyPhone.py
+++ lostMyPhone.py
@@ -1,10 +1,23 @@
 from twilio.rest import Client
-import sys
+from os import path
 
-accountSid = sys.argv[1]
-token = sys.argv[2]
-phone_to = sys.argv[3]
-phone_from = sys.argv[4]
+if path.isfile('./data.txt'):
+    with open('./data.txt', 'r') as file:
+        accountSid = file.readline()
+        token = file.readline()
+        phone_to = file.readline()
+        phone_from = file.readline()
+else:
+    with open('./data.txt', 'r') as file:
+        accountSid = input("AccountSID:")
+        token = input("Token:")
+        phone_to = input("What number would you like to find?")
+        phone_from = input("What is your Twilio phone number?")
+        file.write(accountSid+"\n")
+        file.write(token+"\n")
+        file.write(phone_to+"\n")
+        file.write(phone_from+"\n")
+        
 
 client = Client(accountSid, token)
 
@@ -14,4 +27,4 @@ call = client.calls.create(
     from_=phone_from
 )
 
-print(call.sid)
+print("calling...")
